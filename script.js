626b1338b5430ab71e800c6765027240671a76f5
anshojha <anshojha24@gmail.com>
2022-05-10 23:40:43 +0530

    Today I did some css to make each slid look cool

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 console.log("SCript running");
 document.querySelector('.cross').style.display='none';
 document.querySelector('.hamburger').addEventListener("click",()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebargo');
-    if (document.querySelector('.sidebar').classList.toggle('sidebargo')) {
-        document.querySelector('.ham').style.display='inline'
-        document.querySelector('.cross').style.display='none'
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if (document.querySelector('.sidebar').classList.contains('sidebarGo')) {
+        document.querySelector('.ham').style.display='inline';
+        document.querySelector('.cross').style.display='none';
     }
     else{
-        document.querySelector('.cross').style.display='inline'
-        document.querySelector('.ham').style.display='none'
-    }
-
-})
+        document.querySelector('.ham').style.display='none';
+        setTimeout(() => {
+            document.querySelector('.cross').style.display='inline'; 
+        }, 280);
+    }})
